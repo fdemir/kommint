f3ed843acaf7996128592f746483869438a4c939
fdemir <frndemir@gmail.com>
2024-08-13 05:19:12 +0300

    docs

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Kommint</span>,
+  project: {
+    // link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://kommint.com',
+  },
+  docsRepositoryBase: "https://github.com/kommint",
+  footer: {
+    text: 'Kommint',
+
+  },
+}
+
+export default config
